dd20b149cefc149db8d5648fbd8a5d0f34216602
John Tramm <john.tramm@gmail.com>
2020-08-20 21:52:55 +0000

    added ray tracing kernel file to repo.

---
+++ ray_trace_kernel.c
@@ -0,0 +1,174 @@
+#include "minray.h"
+
+// Boundary surfaces:
+// inside_domain = 0
+// negative_x = 1;
+// positive_x = 3;
+// negative_y = 2;
+// positive_y = 4;
+int find_cell_id(double x, double y, double inverse_cell_width, int n_cells_per_dimension, double inverse_length_per_dimension, int * boundary_surface, int * x_idx, int * y_idx)
+{
+  *x_idx = x * inverse_cell_width;
+  *y_idx = y * inverse_cell_width;
+  //printf("x_idx = %d, y_idx = %d\n", *x_idx, *y_idx);
+
+  int boundary_x = floor(x * inverse_length_per_dimension);
+  int boundary_y = floor(y * inverse_length_per_dimension);
+  //printf("[%lf, %lf]  boundary_x = %d boundary_y = %d inverse_length_per = %lf\n", x, y, boundary_x, boundary_y, inverse_length_per_dimension);
+
+  if( boundary_x )
+    *boundary_surface = boundary_x+2;
+  else if( boundary_y )
+    *boundary_surface = boundary_y+3;
+  else
+    *boundary_surface = 0;
+
+  int cell_id = *y_idx * n_cells_per_dimension + *x_idx; 
+  return cell_id;
+}
+
+double cartesian_ray_trace(double x, double y, double cell_width, int x_idx, int y_idx, double x_dir, double y_dir)
+{
+  x -= x_idx * cell_width;
+  y -= y_idx * cell_width;
+
+  double min_dist = 1e9;
+  //printf("Shifted coord [%lf, %lf]\n", x, y);
+  //printf("X,Y Indices:  [%d, %d]\n", x_idx, y_idx);
+
+  // Test out all 4 surfaces
+  double x_pos_dist = (cell_width - x) / x_dir;
+  double y_pos_dist = (cell_width - y) / y_dir;
+  double x_neg_dist = -x / x_dir;
+  double y_neg_dist = -y / y_dir;
+
+  // Determine closest one
+  if( x_pos_dist < min_dist && x_pos_dist > 0 )
+    min_dist = x_pos_dist;
+  if( y_pos_dist < min_dist && y_pos_dist > 0 )
+    min_dist = y_pos_dist;
+  if( x_neg_dist < min_dist && x_neg_dist > 0)
+    min_dist = x_neg_dist;
+  if( y_neg_dist < min_dist && y_neg_dist > 0)
+    min_dist = y_neg_dist;
+
+  return min_dist;
+}
+
+void print_ray(double x, double y, double x_dir, double y_dir, int cell_id)
+{
+  printf("Location[%.3lf, %.3lf] Direction[%.3lf, %.3lf] Cell ID %d\n", x, y, x_dir, y_dir, cell_id);
+}
+
+#define BUMP 1.0e-10
+void ray_trace_kernel(Parameters P, SimulationData SD, RayData rayData, int ray_id)
+{
+  double distance_travelled = 0.0;
+  int intersection_id = 0;
+  int boundary_surface = 0;
+
+  double x =     rayData.location_x[ray_id];
+  double y =     rayData.location_y[ray_id];
+  double x_dir = rayData.direction_x[ray_id];
+  double y_dir = rayData.direction_y[ray_id];
+  //double z_dir = rayData.direction_z[ray_id];
+  int cell_id =  rayData.cell_id[ray_id];
+  int x_idx = cell_id % P.n_cells_per_dimension;
+  int y_idx = cell_id / P.n_cells_per_dimension;
+
+  int just_hit_vacuum = 0;
+
+  // We run this loop until either:
+  // 1) The maximum number of intersections has been reached
+  // 2) The ray has reached its set distance
+  for( intersection_id = 0; (intersection_id < P.max_intersections_per_ray) && (distance_travelled < P.distance_per_ray); intersection_id++ )
+  {
+    //printf("beginning intersection %d\n", intersection_id);
+    //print_ray(x, y, x_dir, y_dir, cell_id);
+
+    // Find distance to next surface
+    double distance_to_surface = cartesian_ray_trace(x, y, P.cell_width, x_idx, y_idx, x_dir, y_dir);
+    //printf("distance to surface = %.3lf\n", distance_to_surface);
+  
+    // Check to see if we are terminal
+    if(distance_travelled + distance_to_surface >= P.distance_per_ray)
+    {
+      distance_to_surface = (P.distance_per_ray - distance_travelled) + BUMP;
+    }
+  
+    // Record Intersection information
+    uint64_t global_intersection_id = ray_id * P.max_intersections_per_ray + intersection_id;
+
+    // Record distance to next surface
+    SD.readWriteData.intersectionData.distances[global_intersection_id] = distance_to_surface;
+
+    // Record the ID of the cell we are in
+    SD.readWriteData.intersectionData.cell_ids[global_intersection_id] = cell_id;
+    
+    // Record if we hit vacuum
+    SD.readWriteData.intersectionData.did_vacuum_reflects[global_intersection_id] = just_hit_vacuum;
+    just_hit_vacuum = 0;
+    
+    // Record if we hit vacuum
+    // TODO NOTE: If running in parallel, this may need to be an atomic operation.
+    SD.readWriteData.cellData.hit_count[cell_id] = 1;
+
+    // Move ray forward to intersection surface
+    x += x_dir * distance_to_surface;
+    y += y_dir * distance_to_surface;
+
+    double x_across_surface = x + x_dir * BUMP;
+    double y_across_surface = y + y_dir * BUMP;
+    int x_idx_across_surface, y_idx_across_surface;
+    
+    // Look up cell_ID of neighbor we are travelling into
+    int neighbor_id = find_cell_id(x_across_surface, y_across_surface, P.inverse_cell_width, P.n_cells_per_dimension, P.inverse_length_per_dimension, &boundary_surface, &x_idx_across_surface, &y_idx_across_surface);
+    //printf("neighbor_id = %d\n", neighbor_id);
+    
+    //printf("boundary surface = %d\n", boundary_surface);
+    // Reflect
+    if( boundary_surface )
+    {
+      printf("just hit a boundary\n");
+      if( boundary_surface % 2 == 1 )
+        x_dir *= -1.0;
+      else
+        y_dir *= -1.0;
+    }
+
+    // Lookup boundary condition information based on boundary surface hit
+    int boundary_condition = P.boundary_conditions[boundary_surface];
+
+    // Note boundary information for next intersection.
+    if( boundary_condition == VACUUM )
+    {
+      just_hit_vacuum = 1;
+      printf("Just hit vacuum boundary\n");
+    }
+
+    // If we didn't hit a boundary, move into the next cell.
+    if( !boundary_condition )
+    {
+      cell_id = neighbor_id;
+      x_idx = x_idx_across_surface;
+      y_idx = y_idx_across_surface;
+    }
+
+    // Move ray off the surface
+    x += x_dir * BUMP;
+    y += y_dir * BUMP;
+
+    distance_travelled += distance_to_surface;
+    printf("distance_travelled = %lf\n", distance_travelled);
+    //print_ray(x, y, x_dir, y_dir, cell_id);
+  }
+  
+  rayData.location_x[ray_id] = x;
+  rayData.location_y[ray_id] = y;
+  rayData.direction_x[ray_id] = x_dir;
+  rayData.direction_y[ray_id] = y_dir;
+  //rayData.direction_z[ray_id] = z_dir;
+  rayData.cell_id[ray_id] = cell_id;
+    
+  SD.readWriteData.intersectionData.n_intersections[ray_id] = intersection_id;
+}
