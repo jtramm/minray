380611cd60fd608f246a32be3f3b9e1ab5923548
John Tramm <john.tramm@gmail.com>
2020-08-20 18:25:40 +0000

    continuing to work on ray tracing kernel.

--- main.c
+++ main.c
@@ -38,64 +38,6 @@ int main(int argc, char * argv[])
   initialize_rays(P, SD);
   initialize_fluxes(P, SD);
 
-  {
-  double cell_width = P.cell_width;
-  printf("cell_width = %.7lf\n", cell_width);
-  int x_idx = 4;
-  int y_idx = 6;
-  double x = x_idx * cell_width + 0.001;
-  double y = y_idx * cell_width + 0.001;
-  double x_dir = sqrt(2.0) / 2.0;
-  double y_dir = sqrt(2.0) / 2.0;
-  double z_dir = 0;
-
-  double dist = cartesian_ray_trace(x, y, cell_width, x_idx, y_idx, x_dir, y_dir, z_dir);
-  printf("distance = %.5lf\n", dist);
-  }
-  {
-  double cell_width = P.cell_width;
-  printf("cell_width = %.7lf\n", cell_width);
-  int x_idx = 4;
-  int y_idx = 6;
-  double x = x_idx * cell_width + 0.9999 * cell_width;
-  double y = y_idx * cell_width + 0.9999 * cell_width;
-  double x_dir = -sqrt(2.0) / 2.0;
-  double y_dir = -sqrt(2.0) / 2.0;
-  double z_dir = 0;
-
-  double dist = cartesian_ray_trace(x, y, cell_width, x_idx, y_idx, x_dir, y_dir, z_dir);
-  printf("distance = %.5lf\n", dist);
-  }
-  
-  {
-  double cell_width = P.cell_width;
-  printf("cell_width = %.7lf\n", cell_width);
-  int x_idx = 4;
-  int y_idx = 6;
-  double x = x_idx * cell_width + cell_width * 0.5;
-  double y = y_idx * cell_width + 0.001;
-  double x_dir = sqrt(2.0) / 2.0;
-  double y_dir = sqrt(2.0) / 2.0;
-  double z_dir = 0;
-
-  double dist = cartesian_ray_trace(x, y, cell_width, x_idx, y_idx, x_dir, y_dir, z_dir);
-  printf("distance = %.5lf\n", dist);
-  }
-  
-  {
-  double cell_width = P.cell_width;
-  printf("cell_width = %.7lf\n", cell_width);
-  int x_idx = 4;
-  int y_idx = 6;
-  double x = x_idx * cell_width + cell_width * 0.5;
-  double y = y_idx * cell_width + 0.001;
-  double x_dir = 0.00001;
-  double y_dir = 0.99999;
-  double z_dir = 0;
-
-  double dist = cartesian_ray_trace(x, y, cell_width, x_idx, y_idx, x_dir, y_dir, z_dir);
-  printf("distance = %.5lf\n", dist);
-  }
 
 
   return 0;
